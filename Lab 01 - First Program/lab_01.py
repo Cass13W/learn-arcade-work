4f581a87b3b7bbd3ff888d870d716aa44d9a1fcc
Cassie Witt <carrottop468@gmail.com>
2024-01-22 17:39:54 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
 print("Hello!\nMy name is Cassie.\nI'm studying to become a robotics engineer!")
 print("When I was younger, my dad showed me the robots at his job and how they worked.")
-print("Ever since then,I've been fascinated with how people can make nuts and bolts move so realistically!.")
+print("Ever since then,I've been fascinated with how people can make nuts and bolts move so realistically!")
 print("I'm so excited for what the future brings me in this career!")
